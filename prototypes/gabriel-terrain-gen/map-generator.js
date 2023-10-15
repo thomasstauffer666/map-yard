7398f78660a6437179d13a20a1048a409ca544d6
gabriel <gabrielcmoris@gmail.com>
2023-10-15 13:20:36 +0200

    base terrain

--- prototypes/gabriel-terrain-gen/map-generator.js
+++ prototypes/gabriel-terrain-gen/map-generator.js
@@ -1,18 +1,18 @@
 const canvas = document.getElementById("mapCanvas");
+canvas.style.backgroundImage = 'url("/prototypes/gabriel-terrain-gen/terrain-textures/ground-brown.svg")';
 const ctx = canvas.getContext("2d");
 
 const canvasWidth = canvas.width;
 const canvasHeight = canvas.height;
 
-const chunkSize = 1;
+const chunkSize = 0.8;
 
 const noise = new Noise(Math.random());
 
 for (let x = 0; x < canvasWidth; x += chunkSize) {
   for (let y = 0; y < canvasHeight; y += chunkSize) {
     const noiseValue = noise.simplex2(x / 100, y / 100);
-    console.log(noiseValue);
-    const color = noiseValue > 0.5 ? "blue" : "#fff555";
+    const color = noiseValue > 0.1 ? "#00000000" : "blue";
 
     ctx.fillStyle = color;
     ctx.fillRect(x, y, chunkSize, chunkSize);
