cfa9eb44b84964e83d2e9e4ddfb4653f9b065add
gabriel <gabrielcmoris@gmail.com>
2023-10-26 18:13:09 +0200

    3 layers

--- prototypes/gabriel-terrain-gen/map-generator.js
+++ prototypes/gabriel-terrain-gen/map-generator.js
@@ -1,5 +1,5 @@
 const canvas = document.getElementById("mapCanvas");
-canvas.style.backgroundImage = 'url("/prototypes/gabriel-terrain-gen/terrain-textures/ground-brown.svg")';
+canvas.style.backgroundImage = 'url("./terrain-textures/ground-brown.svg")';
 const ctx = canvas.getContext("2d");
 
 const canvasWidth = canvas.width;
@@ -12,8 +12,10 @@ const noise = new Noise(Math.random());
 for (let x = 0; x < canvasWidth; x += chunkSize) {
   for (let y = 0; y < canvasHeight; y += chunkSize) {
     const noiseValue = noise.simplex2(x / 100, y / 100);
-    const color = noiseValue > 0.1 ? "#00000000" : "blue";
-
+    let color;
+    if (noiseValue < -0.8) color = "red";
+    else if (noiseValue < 0) color = "#00000000";
+    else color = "blue";
     ctx.fillStyle = color;
     ctx.fillRect(x, y, chunkSize, chunkSize);
   }
